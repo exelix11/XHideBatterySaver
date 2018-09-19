b62e390ffdbcfcca07b14b73e259a17f4409d87f
exelix <ex.elix@libero.it>
2018-09-19 13:48:10 +0200

    Initial commit

---
+++ app/src/main/java/XHideBatterySaverClass.java
@@ -0,0 +1,28 @@
+package com.xhidebatterysaver.root;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import static de.robv.android.xposed.XposedHelpers.findClass;
+import de.robv.android.xposed.XposedHelpers;
+
+public class XHideBatterySaverClass implements IXposedHookLoadPackage  {
+
+    @Override
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        if(!lpparam.packageName.equals("com.android.systemui"))
+            return;
+        XposedBridge.log("XHideBatterySaver: found target package ");
+        XposedBridge.hookAllConstructors(
+                findClass("com.android.systemui.statusbar.phone.BarTransitions$BarBackgroundDrawable",lpparam.classLoader),
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        int targetVal = XposedHelpers.getIntField(param.thisObject,"mTransparent");
+                        XposedHelpers.setIntField(param.thisObject,"mPowerSaveWarning",targetVal);
+                        XposedBridge.log("XHideBatterySaverClass: Patched ! ");
+                    }
+                });
+    }
+}
